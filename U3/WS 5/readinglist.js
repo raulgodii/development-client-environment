77bf128f7e7196a829287dc47a37f4d2dc3be234
raulgodii <raulgodii13@gmail.com>
2023-10-16 20:44:12 +0200

    update

--- U3/WS 5/readinglist.js
+++ U3/WS 5/readinglist.js
@@ -1,15 +1,15 @@
 class BookList{
-    constructor(){
+    constructor(...books){
         this.booksRead = 0; // Number of books marked as read
         this.booksNotRead = 0; // Number of books marked not read yet
         this.nextBook; // A reference to the next book to read (book object)
         this.currentBook; // A reference to the current book being read (book object)
         this.lastBook; // A reference to the last book read (book object)
-        this.books; // An array of all the Books
+        this.books = books; // An array of all the Books
     }
 
     add(book){
-        this.currentBook = book;
+        this.books.push(book);
     }
 
     finishCurrentBook(){
@@ -17,16 +17,29 @@ class BookList{
         this.currentBook.readDate = Date.now();
         this.lastBook = this.currentBook;
         this.currentBook = this.nextBook;
-        this.nextBook = () => {
-            for()
-        }
+        this.nextBook = this.findNextBookToRead();
     }
 
+    findNextBookToRead() {
+        return this.books.find(book => !book.read);
+    }
+
+    getNumberOfBooksRead() {
+        return this.books.filter(book => book.read).length;
+    }
+
+    getNumberOfBooksNotRead() {
+        return this.books.filter(book => !book.read).length;
+    }
+
+    setNextBookToRead(book) {
+        this.currentBook = book;
+    }
 
 }
 
 class Book{
-    constructor(title, genre, author, readDate = Date.now()){
+    constructor(title, genre, author, readDate = null){
         this.title = title;
         this.genre = genre;
         this.author = author;
