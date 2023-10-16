e5c8f9a0642c27af19eb414479bb48b2be3dca32
raulgodii <raulgodii13@gmail.com>
2023-10-16 20:45:21 +0200

    prueba

--- U3/WS 5/readinglist.js
+++ U3/WS 5/readinglist.js
@@ -47,3 +47,20 @@ class Book{
         this.readDate = readDate;
     }
 }
+
+
+const book1 = new Book('Book 1', 'Fiction', 'Author 1');
+const book2 = new Book('Book 2', 'Non-Fiction', 'Author 2');
+const book3 = new Book('Book 3', 'Mystery', 'Author 3');
+
+const bookList = new BookList();
+bookList.add(book1);
+bookList.add(book2);
+bookList.add(book3);
+
+console.log('Number of books read:', bookList.getNumberOfBooksRead()); // Output: 0
+console.log('Number of books not read:', bookList.getNumberOfBooksNotRead()); // Output: 3
+
+bookList.finishCurrentBook();
+console.log('Number of books read:', bookList.getNumberOfBooksRead()); // Output: 1
+console.log('Number of books not read:', bookList.getNumberOfBooksNotRead()); // Output: 2
