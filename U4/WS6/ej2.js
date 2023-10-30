c21d8245c0211e87b342adbd8784596c4302281d
raulgodii <raulgodii13@gmail.com>
2023-10-30 18:42:35 +0100

    ej2 more or less finished

---
+++ U4/WS6/ej2.js
@@ -0,0 +1,65 @@
+window.onload = () => {
+    checkCookie("colorBg", readCookie("colorBg"));
+    checkCookie("colorP", readCookie("colorP"));
+    checkCookie("fontSize", readCookie("fontSize"));
+
+    colorBg = document.getElementById("colorBg");
+    colorP = document.getElementById("colorP");
+    fontSize = document.getElementById("fontSize");
+    errorFontSize = document.getElementById("errorFontSize");
+    btn = document.getElementById("btn");
+    res = document.getElementById("res");
+    
+    btn.addEventListener("click", (e) => {
+        if(!checkValues()){
+            e.preventDefault();
+        }else{
+            
+            setCookie("colorBg", colorBg.value, 5);
+            setCookie("colorP", colorP.value, 5);
+            setCookie("fontSize", fontSize.value, 5);
+        }
+    });
+
+    deleteCookies = document.getElementById("deleteCookies");
+    deleteCookies.addEventListener("click", () => {
+        deleteCookie("colorBg");
+        deleteCookie("colorP");
+        deleteCookie("fontSize");
+    });
+
+
+    // h1 = document.getElementById("h1");
+    // h1.innerHTML = "¡Bienvenido " + readCookie("usuario") +"!";
+
+    // btn = document.getElementById("btn");
+    // btn.addEventListener("click", () => {
+    //     deleteCookie("usuario");
+    // });
+}
+
+function checkCookie(id, value){
+    if(value != null){
+        document.getElementById(id).disabled = true;
+        res.style.backgroundColor = readCookie("colorBg");
+        res.style.color = readCookie("colorP");
+        res.style.fontSize = readCookie("fontSize") + "px";
+    }
+}
+
+function checkFontSize(cad){
+    let regex = /^[0-9]+$/;
+    res = regex.test(cad);
+    if(!res){
+        errorFontSize.innerHTML = "ERROR: ¡Introduce un tamaño valido!"
+    }else{
+        errorFontSize.innerHTML= "";
+    }
+    return res;
+}
+
+function checkValues(){
+    checkFontSize(fontSize.value);
+
+    return checkFontSize(fontSize.value);
+}
