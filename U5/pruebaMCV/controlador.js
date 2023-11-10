eeaf04433e19081c5a5b623019a3220911db329c
raulgodii <raulgodii13@gmail.com>
2023-11-10 10:44:56 +0100

    mvc

---
+++ U5/pruebaMCV/controlador.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    class Libro{
+        constructor (tit, autor, año){
+            this.titulo = tit;
+            this.autor = autor;
+            this.año = año;
+        }
+    }
+
+    class ListaLectura{
+        constructor(){
+            this.listaLibros = [];
+        }
+
+        addLibro(libro){
+            this.listaLibros.push(libro);
+        }
+    }
+
+}
