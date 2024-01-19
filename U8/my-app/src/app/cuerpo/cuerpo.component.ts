841ca93d4fbe9434e325b76ca1d732e97ba42a83
raulgodii <raulgodii13@gmail.com>
2024-01-19 11:04:24 +0100

    httpclient

--- U8/my-app/src/app/cuerpo/cuerpo.component.ts
+++ U8/my-app/src/app/cuerpo/cuerpo.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
 export class CuerpoComponent {
+  items = ["item1", "item2", "item3", "item4"];
+  criptos :any[] = [];
+  contenidoInput = "";
 
+  constructor(private http: HttpClient){
+
+  }
+
+  peticionAJAX(){
+    this.http.get("https://api.coingecko.com/api/v3/coins/list").subscribe((datos:any)=>{
+      console.log(datos);
+      this.criptos = datos;
+    });
+  }
+
+  nuevoValor(){
+    this.items.push(this.contenidoInput);
+    this.contenidoInput = "";
+  }
+
+  borrarItem(i:any){
+    console.log("borrando")
+    this.items.splice(i, 1);
+  }
 }
