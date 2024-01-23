a646b251babe5850476c7555100f08af7f11a8cb
raulgodii <raulgodii13@gmail.com>
2024-01-23 18:36:40 +0100

    guard

--- U8/my-app/src/app/app.routes.ts
+++ U8/my-app/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { DetalleComponent } from './detalle/detalle.component';
 import { Component } from '@angular/core';
+import { pruebaguardGuard } from './pruebaguard.guard';
 
 export const routes: Routes = [
     {path: 'cuerpo', component: CuerpoComponent},
-    {path: 'detalle/:id', component: DetalleComponent}
+    {path: 'detalle/:id', component: DetalleComponent, canActivate:[pruebaguardGuard]}
 ];
---
+++ U8/my-app/src/app/pruebaguard.guard.ts
@@ -0,0 +1,5 @@
+import { CanActivateFn } from '@angular/router';
+
+export const pruebaguardGuard: CanActivateFn = (route, state) => {
+  return true;
+};
